a90c0b225e5dff7856dc33ede2b31f09e6738697
Vitalij-Burk <burkasovvitalij@gmail.com>
2026-03-01 09:48:19 +0600

    Reconsidered token logic.

--- src/api/token/token_handlers.rs
+++ src/api/token/token_handlers.rs
@@ -91,32 +91,6 @@ pub async fn refresh_token(
 }
 
 #[axum::debug_handler]
-pub async fn revoke_access_token(
-    State(mut state): State<AppState>,
-    Json(access): Json<String>,
-) -> Result<(), (StatusCode, &'static str)> {
-    let public_key = state
-        .key_manager
-        .get_public()
-        .map_err(|error| match error {
-            _ => (StatusCode::INTERNAL_SERVER_ERROR, "Unexpected error"),
-        })?;
-
-    state
-        .token_manager
-        .revoke_access(&access, &public_key)
-        .await
-        .map_err(|error| match error {
-            TokenManagerError::JwtError(_) | TokenManagerError::RedisError(_) => {
-                (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error")
-            }
-            TokenManagerError::NotFound(_) => (StatusCode::UNAUTHORIZED, "User wasn't authorized"),
-        })?;
-
-    Ok(())
-}
-
-#[axum::debug_handler]
 pub async fn revoke_refresh_token(
     State(mut state): State<AppState>,
     Json(refresh): Json<String>,
